e8f163e31477e0ab476423a0c05309acde4c4095
reannu123 <reannumon123@gmail.com>
2023-04-19 20:00:22 +0800

    Fixed logic and added more UI elements

--- src/components/GamePage.tsx
+++ src/components/GamePage.tsx
@@ -28,23 +28,15 @@ function GamePage(props: any) {
   const [canMove, setCanMove] = useState(true);
   const [playerNum, setPlayerNum] = useState(0);
   const [opponentNum, setOpponentNum] = useState(0);
+  const [move, setMove] = useState("");
+  const [showMove, setShowMove] = useState(false);
+  const [showResult, setShowResult] = useState(false);
+  const [win, setWin] = useState(0);
+  const [oppImg, setOppImg] = useState("");
+  const [playerReady, setPlayerReady] = useState(true);
 
   // Interactable functions
-  function leaveQueue() {
-    set(ref(rtdb, "queue/" + props.user.uid), null);
-    setInQueue(false);
-  }
-  function addMove(move: string) {
-    const moves = ref(
-      rtdb,
-      "lobbies/" + lobbyID + "/moves/" + `player${playerNum}`
-    );
-    push(moves, move);
-    set(ref(rtdb, "lobbies/" + lobbyID + "/ready/player" + playerNum), true);
-    setCanMove(false);
-  }
-
-  function addUsertoQueue(user: User) {
+  function joinQueue(user: User) {
     const queue = ref(rtdb, "queue/" + user.uid);
     set(queue, {
       uid: user.uid,
@@ -54,47 +46,140 @@ function GamePage(props: any) {
     });
   }
 
-  useEffect(() => {
-    const lobbies = ref(rtdb, "lobbies/");
-    // Find the lobby that has the user
-    onChildAdded(lobbies, (snapshot) => {
-      const lobby = snapshot.val();
+  function leaveQueue() {
+    set(ref(rtdb, "queue/" + props.user.uid), null);
+    setInQueue(false);
+  }
 
-      if (lobby.player1.uid === props.user.uid) {
-        setInQueue(false);
-        setInGame(true);
-        setOpponent(lobby.player2.email);
-        setLobby(snapshot.key);
-        setPlayerNum(1);
-        setOpponentNum(2);
-      } else if (lobby.player2.uid === props.user.uid) {
-        setInQueue(false);
-        setInGame(true);
-        setOpponent(lobby.player1.email);
-        setLobby(snapshot.key);
-        setPlayerNum(2);
-        setOpponentNum(1);
-      }
-    });
-    onValue(ref(rtdb, "lobbies/" + lobbyID + "/"), (snapshot) => {});
-  }, [props.user]);
+  function leaveGame() {
+    set(ref(rtdb, "lobbies/" + lobbyID), null);
+    setInGame(false);
+    setLobby(null);
+    setOpponent(null);
+    setCanMove(true);
+    setPlayerNum(0);
+    setOpponentNum(0);
+    setMove("");
+    setShowMove(false);
+    setShowResult(false);
+  }
 
+  function addMove(move: any) {
+    const moves = ref(rtdb, "lobbies/" + lobbyID + "/moves/player" + playerNum);
+    setMove(move);
+    push(moves, move);
+    set(ref(rtdb, "lobbies/" + lobbyID + "/ready/player" + playerNum), true);
+    setCanMove(false);
+    setShowMove(true);
+  }
+
+  const [lobbies, loading, error] = useList(ref(rtdb, "lobbies/"));
+
+  // Check if the user has been placed in a lobby
   useEffect(() => {
-    setInterval(() => {
-      onValue(
-        ref(rtdb, "lobbies/" + lobbyID + "/ready/player" + opponentNum),
-        (snapshot) => {
-          if (snapshot.val()) {
-            setCanMove(true);
-            set(
-              ref(rtdb, "lobbies/" + lobbyID + "/ready/player" + playerNum),
-              false
-            );
-          }
+    if (lobbies) {
+      lobbies.forEach((lobby) => {
+        if (lobby.val().player1.uid === props.user.uid) {
+          setInQueue(false);
+          setInGame(true);
+          setOpponent(lobby.val().player2.email);
+          setLobby(lobby.key);
+          setPlayerNum(1);
+          setOpponentNum(2);
+          setOppImg(lobby.val().player2.photourl);
+        } else if (lobby.val().player2.uid === props.user.uid) {
+          setInQueue(false);
+          setInGame(true);
+          setOpponent(lobby.val().player1.email);
+          setLobby(lobby.key);
+          setPlayerNum(2);
+          setOpponentNum(1);
+          setOppImg(lobby.val().player1.photourl);
         }
-      );
-    }, 1000);
-  });
+      });
+    }
+  }, [lobbies]);
+
+  // Check if the lobby still exists in the list of lobbies
+  useEffect(() => {
+    if (lobbyID) {
+      get(ref(rtdb, "lobbies/" + lobbyID)).then((snapshot) => {
+        if (!snapshot.exists()) {
+          setInGame(false);
+          setLobby(null);
+          setOpponent(null);
+          setCanMove(true);
+          setPlayerNum(0);
+          setOpponentNum(0);
+          setMove("");
+          setShowMove(false);
+          setShowResult(false);
+        }
+      });
+    }
+  }, [lobbies]);
+
+  const [ready1, loadingReady1, errorReady1] = useObject(
+    ref(rtdb, "lobbies/" + lobbyID + "/ready/player" + playerNum)
+  );
+  const [ready2, loadingReady2, errorReady2] = useObject(
+    ref(rtdb, "lobbies/" + lobbyID + "/ready/player" + opponentNum)
+  );
+
+  // get my latest move
+  const [myMoves, loadingMyMoves, errorMyMoves] = useList(
+    ref(rtdb, "lobbies/" + lobbyID + "/moves/player" + playerNum)
+  );
+  // get opponent's latest move
+  const [theirMoves, loadingTheirMoves, errorTheirMoves] = useList(
+    ref(rtdb, "lobbies/" + lobbyID + "/moves/player" + opponentNum)
+  );
+  function setReady() {
+    setPlayerReady(false);
+    setShowResult(false);
+    setCanMove(true);
+    setShowMove(false);
+  }
+
+  useEffect(() => {
+    if (ready1?.val() && ready2?.val() && myMoves && theirMoves) {
+      console.log("Opponent's Moves: ", theirMoves);
+
+      setTimeout(() => {
+        let myMove = myMoves[myMoves.length - 1];
+        let theirMove = theirMoves[theirMoves.length - 1];
+
+        // Compare the moves
+        if (myMove.val() === "rock" && theirMove.val() === "scissors") {
+          setWin(true);
+        } else if (myMove.val() === "scissors" && theirMove.val() === "paper") {
+          setWin(true);
+        } else if (myMove.val() === "paper" && theirMove.val() === "rock") {
+          setWin(true);
+        } else {
+          setWin(false);
+        }
+        setShowResult(true);
+      }, 1000);
+
+      // Reset the ready state
+
+      setTimeout(() => {
+        set(
+          ref(rtdb, "lobbies/" + lobbyID + "/ready/player" + playerNum),
+          false
+        );
+      }, 2000);
+      setPlayerReady(true);
+    }
+  }, [
+    ready1,
+    ready2,
+    loadingMyMoves,
+    loadingTheirMoves,
+    loadingReady1,
+    loadingReady2,
+  ]);
 
   //
   //
@@ -125,6 +210,7 @@ function GamePage(props: any) {
               <div className="flex flex-col items-center">
                 <h1>Opponent</h1>
                 <h2>{opponent}</h2>
+                <img src={oppImg} alt="Opponent" className="rounded-full" />
               </div>
             </div>
           </section>
@@ -164,6 +250,66 @@ function GamePage(props: any) {
                     <h1>Waiting for opponent...</h1>
                   )}
                 </div>
+                <div>
+                  <button
+                    className="m-4 bg-teal-600 hover:bg-teal-700 text-white font-medium py-2 px-4 rounded transition-all"
+                    onClick={() => leaveGame()}
+                  >
+                    Leave Game
+                  </button>
+                </div>
+                <div>
+                  {showMove ? (
+                    <h1 className="text-4xl">{move}</h1>
+                  ) : (
+                    <h1 className="text-4xl">Select a move</h1>
+                  )}
+                  {showResult ? (
+                    <>
+                      {win === 1 ? (
+                        <>
+                          Opponent's Move:{" "}
+                          <h1 className="text-4xl text-green-400">
+                            {theirMoves
+                              ? theirMoves[theirMoves.length - 1].val()
+                              : "hello"}
+                          </h1>
+                          <h1 className="text-4xl text-green-400">You win! </h1>
+                        </>
+                      ) : win === 2 ? (
+                        <>
+                          Opponent's Move:{" "}
+                          <h1 className="text-4xl text-red-500">
+                            {theirMoves
+                              ? theirMoves[theirMoves.length - 1].val()
+                              : "hello"}
+                          </h1>
+                          <h1 className="text-4xl text-red-500">You lose!</h1>
+                        </>
+                      ) : (
+                        <>
+                          Opponent's Move:{" "}
+                          <h1 className="text-4xl text-orange-500">
+                            {theirMoves
+                              ? theirMoves[theirMoves.length - 1].val()
+                              : "hello"}
+                          </h1>
+                          <h1 className="text-4xl text-orange-500">Draw!</h1>
+                        </>
+                      )}
+                      {playerReady && (
+                        <button
+                          className="m-4 bg-teal-600 hover:bg-teal-700 text-white font-medium py-2 px-4 rounded transition-all"
+                          onClick={() => {
+                            setReady();
+                          }}
+                        >
+                          Ready
+                        </button>
+                      )}
+                    </>
+                  ) : null}
+                </div>
               </div>
             </div>
           </section>
@@ -177,7 +323,7 @@ function GamePage(props: any) {
       <button
         className="m-4 bg-teal-600 hover:bg-teal-700 text-white font-medium py-2 px-4 rounded transition-all"
         onClick={() => {
-          addUsertoQueue(props.user);
+          joinQueue(props.user);
           setInQueue(true);
         }}
       >
